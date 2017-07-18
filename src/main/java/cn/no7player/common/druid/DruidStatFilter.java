2829c74599c0db177fd296b9ef86d3489724e7d4
张斯腾 <122711693@qq.com>
2017-07-18 17:49:01 +0800

    添加druid

---
+++ src/main/java/cn/no7player/common/druid/DruidStatFilter.java
@@ -0,0 +1,19 @@
+package cn.no7player.common.druid;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * @author zhangst
+ * @create 2017-07-10 17:19
+ */
+
+@WebFilter(filterName="druidWebStatFilter",urlPatterns="/*",
+        initParams={
+                @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
+        })
+public class DruidStatFilter extends WebStatFilter {
+
+}
